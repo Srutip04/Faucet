201ab5fa33a29ec52d176932e5cdc4d131bf4226
Srutip04 <subuhishruti75@gmail.com>
2022-08-19 21:48:27 +0530

    .

--- truffle/contracts/FaucetContract.sol
+++ truffle/contracts/FaucetContract.sol
@@ -2,5 +2,10 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet{
-   receive() external payable {}
+    //this is a special function 
+    // it is called when  you make a tx that dosnt specify func name yo call
+    // external func are part of the contract interface
+    //which means they can be called via contract and other tx
+
+   receive() external payable {} //receive ether
 }
