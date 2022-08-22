d93749b31221f5545bf45dcffd6a5b59280340ed
Srutip04 <subuhishruti75@gmail.com>
2022-08-22 19:55:38 +0530

    getFunders

--- truffle/contracts/FaucetContract.sol
+++ truffle/contracts/FaucetContract.sol
@@ -2,10 +2,19 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet{
-    //this is a special function 
-    // it is called when  you make a tx that dosnt specify func name yo call
-    // external func are part of the contract interface
-    //which means they can be called via contract and other tx
+   address[] public funders;
 
    receive() external payable {} //receive ether
+
+   function addFunds() external payable {
+    funders.push(msg.sender);
+   }
+
+   function getAllFunders() public view returns (address[] memory) {
+    return funders;
+   }
+
+   
+   
+   
 }
