2bdc2865d5c19330def2bffa4986a31c642d2a92
Srutip04 <subuhishruti75@gmail.com>
2022-09-06 22:40:33 +0530

    preventing duplicates

--- truffle/contracts/FaucetContract.sol
+++ truffle/contracts/FaucetContract.sol
@@ -3,36 +3,23 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet{
    uint public numOfFunders;
-   mapping(uint => address) public funders;
-
-   //private -> can be accesible only within the smart contract
-   // internal -> can be accesible only within the smart contract and also derived contracts
+   mapping(address=> bool) private funders;
 
    receive() external payable {} //receive ether
 
    function addFunds() external payable {
-    // funders.push(msg.sender);
-    uint index = numOfFunders++;
-    funders[index] = msg.sender;
-   }
+ 
+    address funder = msg.sender;
 
-   function getAllFunders() public view returns (address[] memory) {
-    address[] memory _funders = new address[](numOfFunders);
-    
-    for(uint i = 0; i < numOfFunders; i++) {
-      _funders[i] = funders[i];
+    if(!funders[funder]){
+        numOfFunders++;
+        funders[funder] = true;
     }
-    return _funders;
-   }
-
-  
-
-   function getFunderAtIndex(uint256 index) external view returns (address) {
-    //  address[] memory _funders = getAllFunders();
-       return funders[index];
    }
 
    
+
+
    
    
 }
