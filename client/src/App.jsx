262b03312d353ea3190d170419fe35f17f3438b4
Srutip04 <subuhishruti75@gmail.com>
2022-09-08 12:37:22 +0530

    enable eth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
 import { EthProvider } from "./contexts/EthContext";
 import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  
+  useEffect(()=>{
+    const web3Provider = async () =>{
+      console.log(window.web3);
+      console.log(window.ethereum);
+      // const web3 = new Web3(window.ethereum);
+      // await window.ethereum.enable();
+      // const accounts = await web3.eth.getAccounts();
+      // console.log(accounts);
+    }
+
+    web3Provider();
+  },[])
+
   return (
     <EthProvider>
       <>
@@ -10,6 +25,17 @@ function App() {
             <div className="balance-view is-size-2">
               Current Balance: <strong>10</strong> ETH
             </div>
+            <button
+              className="btn mr-2"
+              onClick={async () => {
+                const accounts = await window.ethereum.request({
+                  method: "eth_requestAccounts",
+                });
+                console.log(accounts);
+              }}
+            >
+              Enable Ethereum
+            </button>
             <button className="btn mr-2">Donate</button>
             <button className="btn">Withdraw</button>
           </div>
